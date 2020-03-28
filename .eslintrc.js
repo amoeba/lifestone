136cf0c42dc7150b5ebd4707a42d0b8d93027e73
Bryce Mecum <petridish@gmail.com>
2020-03-28 00:29:34 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,12 @@ module.exports = {
     node: true
   },
 
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-
   parserOptions: {
     ecmaVersion: 2020
   },
 
   rules: {
-    "no-console": "on",
+    "no-console": 2,
     "no-debugger": "off"
   },
 
