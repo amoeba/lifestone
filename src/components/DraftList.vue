1e20aee31d261fcca289240051fc9f0f9a732fd9
Bryce Mecum <petridish@gmail.com>
2020-03-28 00:09:32 -0800

    Add text at the top of the drafts view when no drafts

--- src/components/DraftList.vue
+++ src/components/DraftList.vue
@@ -7,7 +7,10 @@
       :index="index"
       :draft="draft"
       @remove="remove"
-    />
+    <p
+      v-if="drafts.length === 0"
+    >No drafts to show. When editing a Weenie, click the "Save Draft" button to save a draft of that Weenie.</p>
+    <Draft v-for="(draft, index) in drafts" :key="index" :index="index" :draft="draft" />
   </div>
 </template>
 
